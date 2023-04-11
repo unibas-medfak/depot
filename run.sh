e3f91d3a58f30e82e6ded234df9381f0da5e7bc7
Martin Imobersteg <martin.imobersteg@gmail.com>
2023-04-11 16:46:06 +0200

    fix docker compose

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-openssl req -x509 -nodes -newkey rsa:2048 -keyout key.pem -out cert.pem -sha256 -days 365 \
+mkdir -p ssl
+
+openssl req -x509 -nodes -newkey rsa:2048 -keyout ssl/key.pem -out ssl/cert.pem -sha256 -days 365 \
     -subj "/C=CH/ST=Basel/L=Basel/O=Unibas/OU=Medizin/CN=localhost"
 
 docker build . -t depot
